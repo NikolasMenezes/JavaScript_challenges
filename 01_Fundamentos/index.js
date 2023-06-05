3289d4a901379eb757abe429e7a0e7869eb941f7
NikolasMenezes <111708104+NikolasMenezes@users.noreply.github.com>
2023-06-05 17:18:20 -0300

    Fibonacci

--- 01_Fundamentos/index.js
+++ 01_Fundamentos/index.js
@@ -67,4 +67,187 @@ apBtn.addEventListener("click", (e) => {
 
   area.innerHTML = areaCalc
   perimetro.innerHTML = perimetroCalc
+
 })
+
+// check if it's divisable
+
+const divisor = 2
+const dividendo = 10
+
+function checaDivisao(a, b){
+  
+  if(a%b != 0){
+    console.log("Não é divisível")
+  } else{
+    console.log("É divisível");
+  }
+}
+
+checaDivisao(dividendo, divisor)
+
+// age group classification
+
+const pessoa1 = 10
+const pessoa2 = 17
+const pessoa3 = 40
+const pessoa4 = 65
+
+function ageGroup (idade){
+  
+  if(idade <= 12){
+    console.log("Faixa etária: Criança")
+  } 
+  else  if(idade <= 17){
+    console.log("Faixa etária: Adolescente")
+  } 
+  else if(idade <= 59){
+    console.log("Faixa etária: Adulto")
+  }
+  else{
+    console.log("Faoxa etária: Idoso")
+  }
+
+}
+
+ageGroup(pessoa1)
+ageGroup(pessoa2)
+ageGroup(pessoa3)
+ageGroup(pessoa4)
+
+// function which compares numbers
+
+let anyNumberA = 54
+let anyNumberB = 34
+
+const comparingNumber = (a, b) =>{
+
+  if (a > b){
+    console.log("O Elemento A é maior que o B")
+  }
+  else if (b > a) {
+    console.log("O Elemento B é maior que o A")    
+  }
+  else{
+    console.log("Os elementos são iguais")
+  }
+}
+
+comparingNumber(anyNumberA, anyNumberB)
+
+// calculator
+
+let operation = 'subtração'
+
+const valorSwitch = 20
+const valorSwitch2 = 37
+
+const somaSwitch = (a,b) =>{
+  return a + b
+}
+const subtracaoSwitch = (a,b) =>{
+  return a - b
+}
+const multiplicacaoSwitch = (a,b) =>{
+  return a * b
+}
+const divisaoSwitch = (a,b) =>{
+  return a / b
+}
+
+switch (operation){
+  case 'soma': {
+    console.log(somaSwitch(valorSwitch, valorSwitch2))
+    break
+  }
+  case 'subtração': {
+    console.log(subtracaoSwitch(valorSwitch, valorSwitch2))
+    break
+  }
+  case 'multiplicação': {
+    console.log(multiplicacaoSwitch(valorSwitch, valorSwitch2))
+    break
+  }
+  case 'divisão': {
+    console.log(divisaoSwitch(valorSwitch, valorSwitch2))
+  }
+  break
+}
+
+// Costs by age
+
+function calculaTarifa(idade, estudante = false){
+
+  if(idade <= 6){
+    console.log("Crianças menores de 6 anos não pagam tarifa.")
+  }
+  else if(idade > 6 && estudante){
+    console.log("Pagará a metade da tarifa normal: R$:1,25")
+  }
+  else if (idade >= 60){
+    console.log(`Pagará com 30% de desconto`)
+  } 
+  else{
+    console.log("Pagará a tarifa normal: R$:2,50")
+  }
+
+}
+
+calculaTarifa(60) // 30% desconto
+calculaTarifa(5) // isento
+calculaTarifa(10, true) // 50% desconto
+calculaTarifa(30) // normal
+calculaTarifa(70, true) // 50% desconto
+
+// loop exercise
+
+const n = 5
+
+for(let i=1; i<n; i++){
+  console.log(i)
+}
+
+// multiplication table
+
+console.log("| Multiplication table |")
+
+let multiplicator = 6
+
+for(let i = 1; i < 11; i++){
+  console.log(i*multiplicator)
+}
+
+// cousin number
+
+console.log("| Exercício números primos |");
+
+for(let i = 0; i<30; i++){
+  if (i > 2){
+    console.log(i);
+  }
+}
+
+
+// printing patterns
+
+console.log('| Printing Patterns |');
+
+const maxNumber = 5
+
+for(let i = 1; i <= maxNumber; i++){
+  console.log("*".repeat(i))
+  
+  if(i == maxNumber){
+    for(let j = maxNumber; j >= 0; j--){
+      console.log("*".repeat(j))
+    }
+  }
+
+}
+
+// fibonacci
+
+const targetNumber = 12
+const fibonacci = []
+
+
