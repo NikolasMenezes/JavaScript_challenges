1234b6245cad6104cc933fe4559a1b670565b15f
NikolasMenezes <111708104+NikolasMenezes@users.noreply.github.com>
2023-06-22 16:08:46 -0300

    Search Mecanism

--- 04_DOM/scripts.js
+++ 04_DOM/scripts.js
@@ -24,6 +24,51 @@ const title = document.querySelector("title").textContent = "JavaScript Challeng
 const divEstilizado = document.querySelector(".div-estilizada")
 const btnDiv = document.querySelector(".btn-div")
 
-btnDiv.addEventListener("click", ()=>{
+btnDiv.addEventListener("click", () => {
   divEstilizado.classList.toggle('ativo')
 })
+
+// Clonando elementos
+
+const list = document.querySelector(".list-clone")
+const buutonList = document.querySelector(".button-list")
+
+
+function clonar() {
+  const elemento = list.querySelector("li")
+  const elementoClonado = elemento.cloneNode(true)
+  elementoClonado.innerText = 'elemento clonado'
+
+  list.appendChild(elementoClonado)
+}
+
+buutonList.addEventListener("click", clonar)
+
+// Removendo itens
+
+const items = document.querySelectorAll(".list-remove li")
+
+items.forEach(item => item.addEventListener("click", () => {
+  item.remove()
+}))
+
+// Mecanismo de busca
+
+const input  = document.querySelector(".search")
+const searchButton  = document.querySelector(".search-button")
+const searchList  = document.querySelectorAll(".search-list li")
+
+const verificaCompatibilidade = ()=>{
+  const content = input.value
+  searchList.forEach(item => {
+    if(content === ''){
+      item.classList.remove("hide")
+    }
+    else if(!(item.innerText === content)){
+      item.classList.add("hide")
+    }
+  })
+
+}
+
+searchButton.addEventListener("click", verificaCompatibilidade)
